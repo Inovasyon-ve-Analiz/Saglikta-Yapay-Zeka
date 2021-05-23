62c97803f27e8d401869d668ad11e3497ebf7507
gencf <79080187+gencf@users.noreply.github.com>
2021-05-23 08:27:05 +0300

    furkan

--- augmentation.py
+++ augmentation.py
@@ -7,14 +7,18 @@ def augmentation(img_dirs):
         for i, filename in enumerate(files):
             input_path = os.path.join("TRAINING",filename)
             output_path = os.path.join(dir,filename)
+            if os.path.isfile(output_path) == False:
+                os.mkdir(output_path)
             img = cv2.imread(input_path, 0)
+            os.chdir("..")
             if dir == "rotated":
-                image = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+                image = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
             cv2.imwrite(output_path, image)
+            os.chdir("..")
             k = cv2.waitKey(0)
             if k == ord("q"):
                 break
-        os.chdir("..")
+
 
     cv2.destroyAllWindows()
 
