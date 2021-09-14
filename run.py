483271291bff4b35cf5b5040b88d77ae06a7f709
gencf <79080187+gencf@users.noreply.github.com>
2021-09-15 00:45:46 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ number_of_epoch = 1000
 train_dir = "new_dataset/train"
 test_dir = "new_dataset/test"
 save_path = "snapshots"
-batch_size = 16
+batch_size = 32
 binary_classification = True
-checkpoint = 10
+checkpoint = 20
 
 run(lr=lr, wd=wd, number_of_epoch=number_of_epoch, train_dir=train_dir, test_dir=test_dir,save_path=save_path, batch_size=batch_size, binary_classification=binary_classification, checkpoint=checkpoint)
