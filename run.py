9dee0b4e67b32dea8d2efa3b38e1483a8302a2ac
gencf <79080187+gencf@users.noreply.github.com>
2021-09-15 00:24:41 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,11 +4,12 @@ from functions import run
 
 lr = 1e-4
 wd = 0
-number_of_epoch = 20
-train_dir = "new_dataset/train/PNG"
-test_dir = "new_dataset/test/PNG"
+number_of_epoch = 1000
+train_dir = "new_dataset/train"
+test_dir = "new_dataset/test"
 save_path = "snapshots"
-batch_size = 4
+batch_size = 16
 binary_classification = True
+checkpoint = 10
 
-run(lr=lr, wd=wd, number_of_epoch=number_of_epoch, train_dir=train_dir, test_dir=test_dir,save_path=save_path, batch_size=batch_size, binary_classification=binary_classification)
+run(lr=lr, wd=wd, number_of_epoch=number_of_epoch, train_dir=train_dir, test_dir=test_dir,save_path=save_path, batch_size=batch_size, binary_classification=binary_classification, checkpoint=checkpoint)
