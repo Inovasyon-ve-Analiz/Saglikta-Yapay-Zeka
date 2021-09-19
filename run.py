b9750af8520f12ccdc1d03ec5de6775a0a7fee84
gencf <79080187+gencf@users.noreply.github.com>
2021-09-19 14:23:16 +0300

    Update run.py

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ save_path = "snapshots"
 batch_size = 16
 binary_classification = True
 checkpoint = 5
-model_dir = "model_dir.pth"
+model_dir = "/kaggle/input/models/180.pth"
 
 run(lr=lr, wd=wd, number_of_epoch=number_of_epoch, train_dir=train_dir, test_dir=test_dir,save_path=save_path, batch_size=batch_size, binary_classification=binary_classification, checkpoint=checkpoint, model_dir=model_dir)
