5693c40ff77eaeca9fc996ffbab773dfd2d641e5
ienesb <ienes9625@gmail.com>
2021-09-22 11:34:20 +0300

    add sens to accuracy

--- accuracy.py
+++ accuracy.py
@@ -1,6 +1,11 @@
 with open("asama1.txt","r") as file:
     lines = file.readlines()
     correct = 0
+    TP = 0
+    TN = 0
+    FP = 0 
+    FN = 0
+
     for i,line in enumerate(lines[1:]):
         label = line[len(line)-2:len(line)-1]
        
@@ -8,11 +13,24 @@ with open("asama1.txt","r") as file:
         if line[:-1].split(" ")[0][:2] == "IN":
             if label == "0":
                 correct +=1
+                TN += 1
+            else:
+                FP += 1
+                
 
         else:
             if label == "1":
                 correct +=1
+                TP += 1
+            else:
+                FN += 1
+
+    sensitivity = TP/(TP+FN)
+    specificity = TN/(FP+TN)
     
     
 print(correct)
+print(sensitivity)
+print(specificity)
+print((sensitivity+specificity)/2)
                    
